8fecd96e584ac942c15e80bbb88bc7cd6ce9b16d
NamGyuKK <kangng0806@gmail.com>
2023-08-23 18:12:55 +0900

    0823 [database연동 예제 1번 완료]

---
+++ src/main/java/com/uilangage/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,26 @@
+package com.uilangage.spring.ex.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uilangage.spring.ex.database.domain.UsedGoods;
+import com.uilangage.spring.ex.database.repository.UsedGoodsRepository;
+
+//로직 (business) 담당
+@Service
+public class UsedGoodsService {
+	
+	
+	@Autowired
+	private UsedGoodsRepository usedGoodsRepository;
+	//used_goods 모든 데이터 조회
+	public List<UsedGoods> getUsedGoodsList() {
+		
+		List<UsedGoods> usedGoodsList = usedGoodsRepository.selectUsedGoodsList();
+		return usedGoodsList;
+		
+	}
+	
+}
