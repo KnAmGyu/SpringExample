49595c31fc3502e9146616b307029cf422d590c7
NamGyuKK <kangng0806@gmail.com>
2023-08-28 18:49:05 +0900

    [학원] 예제

---
+++ src/main/java/com/uilangage/spring/ex/jsp/service/UserService.java
@@ -0,0 +1,26 @@
+package com.uilangage.spring.ex.jsp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uilangage.spring.ex.jsp.domain.User;
+import com.uilangage.spring.ex.jsp.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public int addUser(String name, String birthday, String email) {
+		int count = userRepository.insertUser(name, birthday, email);
+		return count;
+	}
+	
+	public User getLastUser(){
+		User user =userRepository.selectLastUser();
+		
+		return user;
+	}
+	
+}
